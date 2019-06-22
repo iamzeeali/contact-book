91fba7a672c9f989730a1bdb5104313b5ac93720
unknown <mdzeeshanali93@gmail.com>
2019-06-22 10:40:25 +0530

    Added node and npm versions in package.json

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext';
-import ContactContext from '../../context/contact/contactContext';
+import ContactContext from '../../context/contact/contactContext.js';
 
 const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
